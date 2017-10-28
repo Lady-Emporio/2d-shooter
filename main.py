44e3fa0ec2bf8e73c4f3caf1cd29659444360e34
Lady <godod4@gmail.com>
2017-10-28 12:31:39 +0300

    rotate hero but one display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import pyganim
+import math
 from game_map import Platform,Camera,camera_configure
 from unit import Hero,Hero_body
 WIN_WIDTH = 800
@@ -21,7 +22,7 @@ entities.add(hero)
 entities.add(hero_body)
 
 
-level=open("./Python_copy_paste.txt","r").read().split('\n')
+level=open("./Python_copy_paste1.txt","r").read().split('\n')
 
 PLATFORM_WIDTH=32
 PLATFORM_HEIGHT=32
@@ -30,7 +31,6 @@ for row in level: # вся строка
 	for col in row: # каждый символ
 		if col != " ":
 			pf = Platform(x,y)
-			print(x,y)
 			entities.add(pf)
 			#platforms.append(pf)
 			platforms.add(pf)
@@ -44,6 +44,14 @@ camera = Camera(camera_configure, total_level_width, total_level_height)
 left=right=top=bottom=False
 timer = pygame.time.Clock()
 end_game=False
+
+
+
+
+# class Vec2d(pygame.math.Vector2):
+# 	def __init__(self, x_or_pair, y = None)
+# 	pygame.math.Vector2.__init__(self, x_or_pair, y = None)
+
 while not end_game:
 	timer.tick(60)
 	for e in pygame.event.get():
@@ -57,7 +65,6 @@ while not end_game:
 		   right = False
 		if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
 			left = False  
-
 		if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
 			top = True
 		if e.type == pygame.KEYDOWN and e.key == pygame.K_DOWN:
@@ -71,6 +78,39 @@ while not end_game:
 	hero.update(left, right,top=top,bottom=bottom)
 	hero_body.update(left, right,top=top,bottom=bottom)
 	
+
+
+
+	m_x,m_y=pygame.mouse.get_pos()
+	# wtf=((hero_body.rect.x,m_x),(hero_body.rect.y,m_y))
+	side_a=m_x-hero_body.rect.x
+	side_b=m_y-hero_body.rect.y
+	# if side_a<0:
+	# 	side_a=0
+	# if side_b<0:
+	# 	side_b=0
+	# hypot_to_herobody_and_mouse=math.hypot(side_a,side_b ) 
+	# c=hypot_to_herobody_and_mouse
+	# try:
+	# 	w=math.acos((side_a ** 2 - side_b ** 2 - c ** 2)/(-2 * side_b * c)) #26
+	# except ZeroDivisionError as h:
+	# 	w=0
+	# hero_body.image=pygame.transform.rotate(pygame.image.load("./textures/skins/blue_man/body.png"),math.degrees(w)-180)
+	angleA_X, angleA_Y = (hero_body.rect.x+(hero_body.image.get_width()/2), hero_body.rect.y+(hero_body.image.get_height()/2))
+	angleB_X, angleB_Y = (m_x, m_y)
+
+	a = pygame.math.Vector2(angleA_X, angleA_Y)
+	b = pygame.math.Vector2(angleB_X, angleB_Y)
+
+	zero = pygame.math.Vector2()
+
+	#print( zero.angle_to(a-b) )
+
+	hero_body.image=pygame.transform.rotate(pygame.image.load("./textures/skins/blue_man/body.png"),90-zero.angle_to(a-b))
+
+
+
+
 
 	screen.blit(bg, (0,0))
 	camera.update(hero)
--- unit.py
+++ unit.py
@@ -29,6 +29,7 @@ class Hero(Base):
 	def __init__ (self,*arg):
 		Base.__init__(self,*arg)
 		#self.image_real=pygame.image.load("./textures/skins/blue_man/body.png")
+
 		self.image=pygame.image.load("./textures/skins/blue_man/body.png")
 		self.image.fill((123,123,123,0))
 		self.leg="./textures/skins/blue_man/leg.png"
@@ -69,6 +70,12 @@ class Hero_body(Base):
 	def __init__ (self,*arg):
 		Base.__init__(self,*arg)
 		self.image=pygame.image.load("./textures/skins/blue_man/body.png")
+		#print(dir(self.image))
+		print(self.image.get_width())
+		print(self.image.get_height())
+		
+		#['__class__', '__copy__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '_pixels_address', 'blit', 'convert', 'convert_alpha', 'copy', 'fill', 'get_abs_offset', 'get_abs_parent', 'get_alpha', 'get_at', 'get_at_mapped', 'get_bitsize', 'get_bounding_rect', 'get_buffer', 'get_bytesize', 'get_clip', 'get_colorkey', 'get_flags', 'get_height', 'get_locked', 'get_locks', 'get_losses', 'get_masks', 'get_offset', 'get_palette', 'get_palette_at', 'get_parent', 'get_pitch', 'get_rect', 'get_shifts', 'get_size', 'get_view', 'get_width', 'lock', 'map_rgb', 'mustlock', 'scroll', 'set_alpha', 'set_at', 'set_clip', 'set_colorkey', 'set_masks', 'set_palette', 'set_palette_at', 'set_shifts', 'subsurface', 'unlock', 'unmap_rgb']
+		#self.image=pygame.transform.rotate(pygame.image.load("./textures/skins/blue_man/body.png"),90)
 	def update(self,  left, right,top,bottom):
 		if top:
 			pos_y=-self.move
@@ -84,3 +91,9 @@ class Hero_body(Base):
 			pos_y=0
 		self.rect.y += pos_y
 		self.rect.x += pos_x
+
+
+# class Weapons():
+# 	self.weapons=pygame.image.load("./textures/weapons/pistol/gun.png")
+# 	self.BearWeapon = PygAni(  ( (self.weapons,600),) )
+# 	self.BearWeapon.play()
